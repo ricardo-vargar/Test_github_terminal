b1012ff1845ba47386fa86ed91a10a36e179f9dc
ricardo-vargar <ricardo@8alpha.ai>
2025-09-10 13:52:07 -0600

    sotelo game with bot

--- test.py
+++ test.py
@@ -12,13 +12,33 @@ class SoteloGame:
         config = self.DIFFICULTY_LEVELS.get(difficulty, self.DIFFICULTY_LEVELS['easy'])
         self.size = config['size']
         self.num_obstacles = config['obstacles']
+        self.reset_board()
+        while not self.has_valid_path():
+            self.reset_board()
+        self.move_count = 0
+
+    def reset_board(self):
         self.board = [['.' for _ in range(self.size)] for _ in range(self.size)]
         self.player_pos = (0, 0)
         self.goal_pos = (self.size-1, self.size-1)
         self.board[self.player_pos[0]][self.player_pos[1]] = 'P'
         self.board[self.goal_pos[0]][self.goal_pos[1]] = 'G'
         self.obstacles = self.generate_obstacles()
-        self.move_count = 0
+
+    def has_valid_path(self):
+        def dfs(x, y, visited):
+            if (x, y) == self.goal_pos:
+                return True
+            visited.add((x, y))
+            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                nx, ny = x + dx, y + dy
+                if (0 <= nx < self.size and 0 <= ny < self.size and 
+                    self.board[nx][ny] != '#' and (nx, ny) not in visited):
+                    if dfs(nx, ny, visited):
+                        return True
+            return False
+        
+        return dfs(self.player_pos[0], self.player_pos[1], set())
 
     def generate_obstacles(self):
         obstacles = set()
@@ -92,4 +112,67 @@ def main():
 if __name__ == "__main__":
     main()
 
-    
+
+    class SoteloBot:
+        def __init__(self, game):
+            self.game = game
+
+        def find_path(self):
+            def heuristic(pos):
+                return abs(pos[0] - self.game.goal_pos[0]) + abs(pos[1] - self.game.goal_pos[1])
+
+            def get_neighbors(pos):
+                neighbors = []
+                for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    nx, ny = pos[0] + dx, pos[1] + dy
+                    if (0 <= nx < self.game.size and 0 <= ny < self.game.size and 
+                        self.game.board[nx][ny] != '#'):
+                        neighbors.append((nx, ny))
+                return neighbors
+
+            start = self.game.player_pos
+            goal = self.game.goal_pos
+            frontier = [(heuristic(start), 0, start, [])]
+            visited = set()
+
+            while frontier:
+                _, cost, current, path = min(frontier)
+                frontier.remove((heuristic(current), cost, current, path))
+
+                if current == goal:
+                    return path
+
+                if current in visited:
+                    continue
+
+                visited.add(current)
+
+                for next_pos in get_neighbors(current):
+                    if next_pos not in visited:
+                        new_path = path + [next_pos]
+                        new_cost = cost + 1
+                        frontier.append((new_cost + heuristic(next_pos), new_cost, next_pos, new_path))
+
+            return []
+
+        def get_move(self, current, next_pos):
+            dx = next_pos[0] - current[0]
+            dy = next_pos[1] - current[1]
+            if dx == -1: return 'up'
+            if dx == 1: return 'down'
+            if dy == -1: return 'left'
+            if dy == 1: return 'right'
+            return None
+
+        def play(self):
+            path = self.find_path()
+            if not path:
+                print("No solution found!")
+                return
+            
+            current = self.game.player_pos
+            for next_pos in path:
+                move = self.get_move(current, next_pos)
+                self.game.move_player(move)
+                self.game.display_board()
+                current = next_pos
